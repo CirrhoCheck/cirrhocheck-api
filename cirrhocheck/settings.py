fcb877e1a8cc35f12f6c6d1cff51476da416a3fb
Emily Rumia Naomi <emilyrumiaa@gmail.com>
2024-02-04 23:43:20 +0700

    add automation run tests

--- cirrhocheck/settings.py
+++ cirrhocheck/settings.py
@@ -108,9 +108,9 @@ DATABASES = {
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT', '5432'),
-        # 'TEST': {
-        #      'MIRROR': 'default',
-        # },
+        'TEST': {
+             'MIRROR': 'default',
+        },
     }
 }
 
