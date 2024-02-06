1677e2b413ef29f6f955336c0f6a1e3396d0458a
rifqifarelmuhammad <rifqifarel222@gmail.com>
2024-02-06 17:34:33 +0700

    improved data seed and dummy items test

--- administration/tests/test_dummy_item.py
+++ administration/tests/test_dummy_item.py
@@ -5,5 +5,13 @@ from django.test import TestCase
 class HelloWorldTestCase(TestCase):
     def test_hello_world(self):
         response = self.client.get('/')
+
+        helloWorldIndex = 0
+        for data in response.json():
+            if data['description'] == 'Hello World':
+                break
+            
+            helloWorldIndex += 1
+
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json()[0]['description'], "Hello World")
+        self.assertEqual(response.json()[helloWorldIndex]['description'], "Hello World")
